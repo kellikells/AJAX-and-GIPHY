c1a1e68a6619fc029b8861b39da8cb3cc037401b
K Uyeda <kellikells@gmail.com>
2020-02-02 22:45:49 -0800

    buttons are displayed with attr, but ajax call isn't working

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,41 +1,67 @@
 
 
-var establishedArr = ['Naruto', 'Marvel', 'Looney' + ' Tunes', 'Friends'];
-// example with search: naruto, limit 10, rating G, language en + api_key
-// var queryURL = "https://api.giphy.com/v1/gifs/search?q=naruto&limit=10&rating=G&lang=en&api_key=d20zgadSnjTzFYdOaeNTybFyDwS0MmIS"
+var topics = ['Naruto', 'Marvel', 'Looney' + ' Tunes', 'Friends'];
 
+var imageContainer = $(".image-container");
 
-// loop through preset array & build buttons on DOM
+// LOOP: through topics array & build buttons on DOM
 // =====================================================
-for (let i = 0; i < establishedArr.length; i++) {
-    var myButton = $("<button>");
-    var thisShowIs = establishedArr[i];
+for (let i = 0; i < topics.length; i++) {
+
+    var myButton = $("<button>")
+
+    //----button attr------
     myButton.addClass("established-buttons");
-    //   myButton.attr({
-    //       "rating": 
-    //   })
-    myButton.text(establishedArr[i]);
+    myButton.text(topics[i]);
+    myButton.attr("data-topic", topics[i]);
 
+    //------putting buttons on DOM-----
+    $(".buttonWrapper").append(myButton);
+}
 
-    var GIPHYSresponse = $(this).response;
+//============= FUNCTION: BUTTON ON CLICK ===============
+//--------------------------------------------------------
+$("button").on("click", function () {
 
-    // "host + endpoint: search + paramenters: q, limit, rating G + apiKey"
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + thisShowIs + "&limit=10&rating=G&lang=en&api_key=d20zgadSnjTzFYdOaeNTybFyDwS0MmIS";
+    //---use the "data-topic" to get the button text
+    var tvShow = $(this).attr("data-topic");
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + tvShow + "&limit=10&lang=en&api_key=d20zgadSnjTzFYdOaeNTybFyDwS0MmIS";
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
+        console.log(response);    // logging the resulting object
+
+        var resultsArray = response.data;
+
+        //==============LOOP through GIFs==================
+        //---------GIF info: title, rating, small-still, small-mp4
+        //------------------------------------------------
+        for (let x = 0; x < resultsArray.length; x++) {
+
+            console.log(resultsArray.length);      //logs the number of gifs returned from AJAX call
+            var theTitle = resultsArray[x].title;
+            console.log(theTitle);      //checking to see it logs all 10 titles, it works
+            var theRating = resultsArray[x].rating;
+            var theStill = resultsArray[x].images.fixed_height_small_still;
+            var theMp4 = resultsArray[x].images.fixed_height_small;
+
+            //=========== Appending these to DOM ================
+            // var imageElement = $("<img " + "src=" + theStill + " data-still=" + theStill + " data-animate=" + theMp4 + "data-state= still>")
+            var imageRating = $("<p>" + theRating + "</p>");
+
+            imageContainer.append(imageRating);
+
+        }
     });
 
 
-
-    $(".buttonWrapper").append(myButton);
-}
+})
 
 
-// $(".establishedArr").on('click', function() {
+// $(".topics").on('click', function() {
 //     var x = $()}
 
 
